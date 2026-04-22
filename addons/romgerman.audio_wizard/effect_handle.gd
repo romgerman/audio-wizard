a01a92e2a7461685fe54170fb2db894e7f972d6a
German <romgerman@yandex.ru>
2026-04-22 23:59:26 +0300

    fix: EffectHandle was throwing errors after deleting an effect from a bus

--- addons/romgerman.audio_wizard/effect_handle.gd
+++ addons/romgerman.audio_wizard/effect_handle.gd
@@ -4,11 +4,15 @@ extends RefCounted
 var audio_bus_index := -1
 var audio_eff_index := -1
 
+var effect_cached: AudioEffect
+
 func has_effect() -> bool:
 	return audio_bus_index != -1 and audio_eff_index != -1
 
 func get_effect() -> AudioEffect:
-	return AudioServer.get_bus_effect(audio_bus_index, audio_eff_index)
+	if not effect_cached:
+		effect_cached = AudioServer.get_bus_effect(audio_bus_index, audio_eff_index)
+	return effect_cached
 
 static func create(bus_index: int, eff_index: int):
 	var handle := new()
